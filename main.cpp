80e5f633c0a9ff06aae65aad86064e9fea5eef57
Залесский Семён Сергеевич и Юкленчук Кирилл Андреевич ИП-515 <s@localhost.localdomain>
2025-10-02 14:16:29 +0700

    исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 int main(int argc, char** argv){
     char* str = (char*)malloc(sizeof(char)*SIZE);
     printf("ВВЕДИТЕ СВОЕ ИМЯЯЯЯЯЯЯЯЯЯЯЯ:\n");
+<<<<<<< HEAD
     str=gets(str);
     printf("привет, %s", str);
+=======
+    scanf("%s", str);
+    puts("привет");
+    puts(str);
+>>>>>>> ecd576b (исправлено приветствие)
     return 0;
 }
